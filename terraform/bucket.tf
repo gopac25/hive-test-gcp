18ba485ce6a2a46135a29c9298a26bdabe33339f
Gopala Krishnan Chellappa <gopalakrishnan.chellappa@voyd.tv>
2023-03-05 22:24:54 +0100

    Bucket Creation

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -1,7 +1,7 @@
 # Create a GCS Bucket
 
 resource "google_storage_bucket" "hive" {
-  name          = "var.bucket_name"
+  name          = var.bucket_name
   force_destroy = false
   location      = "EU"
   storage_class = "STANDARD"

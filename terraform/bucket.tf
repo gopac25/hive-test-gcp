70fdf2052407b29b0c27906074176a8253d13584
Gopala Krishnan Chellappa <gopalakrishnan.chellappa@voyd.tv>
2023-03-05 22:11:45 +0100

    bucket creation and source code

---
+++ terraform/bucket.tf
@@ -0,0 +1,11 @@
+# Create a GCS Bucket
+
+resource "google_storage_bucket" "hive" {
+  name          = "var.bucket_name"
+  force_destroy = false
+  location      = "EU"
+  storage_class = "STANDARD"
+  versioning {
+    enabled = true
+  }
+}
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,2 @@
+project_id  = "hive-test-gcp" # GCP Project ID.
+bucket_name = "tfstate"    # GCS Bucket name.
